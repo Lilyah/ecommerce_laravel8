61ee7816813c17e56dfc02d5890c5b0e75177a31
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2022-09-06 14:22:43 +0300

    resources/views/auth/forgot-password.blade & forgot-password-original-laravel.blade: Setuped custom forget password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,56 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('frontend.frontend_master')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@section('content')
+
+
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="{{ url('/') }}">Home</a></li>
+				<li><a href="{{ route('login') }}">Login</a></li>
+				<li class='active'>Forget Password</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content">
+	<div class="container">
+		<div class="sign-in-page">
+			<div class="row">
+				<!-- Sign-in -->			
+<div class="col-md-6 col-sm-6 sign-in">
+
+    <h4 class="">Forget Password</h4>
+	<p class="">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+
+    @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
         </div>
+    @endif
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
 
-        <x-jet-validation-errors class="mb-4" />
+		<div class="form-group">
+		    <label class="info-title" for="email">Email Address <span>*</span></label>
+		    <input type="email" class="form-control unicase-form-control text-input" name="email" id="email" :value="old('email')" required autofocus>
+		</div>
+	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Send</button>
+	</form>					
+</div>
+<!-- Forget Password -->
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+        @include('frontend.body.brands')
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+<!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	
+</div><!-- /.container -->
+</div><!-- /.body-content -->
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+
+@endsection
