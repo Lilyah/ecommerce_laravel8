c7966b3fef1af4a329235c657fefaeb1bf37c2d3
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2022-09-07 13:04:10 +0300

    app/Http/Controllers/Frontend/IndexController: Developed UserLogout() method

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     // Load Home Page
     public function index(){
         return view('frontend.index');
+    }
+
+    // User Logout
+    public function UserLogout(){
+        Auth::logout(); // logout is default method
+        return redirect()->route('login');
     }
 }
