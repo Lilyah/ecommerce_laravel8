1b3b0fb45fc385c9f8b3751555982b0d8187bed9
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2022-09-14 12:41:04 +0300

    views/backend/brands/brands_view.blade: Adding the main html table and displaying the results for all brands from db

--- resources/views/backend/brands/brands_view.blade.php
+++ resources/views/backend/brands/brands_view.blade.php
@@ -2,5 +2,56 @@
 
 @section('admin')
 
+	  <div class="container-full">
+
+		<!-- Main content -->
+		<section class="content">
+		  <div class="row">
+
+			<div class="col-12">
+
+			 <div class="box">
+				<div class="box-header with-border">
+				  <h3 class="box-title">Brands List</h3>
+				</div>
+				<!-- /.box-header -->
+				<div class="box-body">
+					<div class="table-responsive">
+					  <table id="example1" class="table table-bordered table-striped">
+						<thead>
+							<tr>
+                                <td>Brand EN</td>
+								<td>Brand BG</td>
+								<td>Image</td>
+								<td>Action</td>
+							</tr>
+						</thead>
+						<tbody>
+                            @foreach ($brands as $brand)
+							<tr>
+								<td>{{ $brand->brand_name_en }}</td>
+								<td>{{ $brand->brand_name_bg }}</td>
+								<td><img src="{{ asset($brand->brand_image) }}" style="width: 70px; height: 40px;"></td>
+								<td>
+                                    <a href="" class="btn btn-info">Edit</a>
+                                    <a href="" class="btn btn-danger">Delete</a>
+                                </td>
+							</tr>
+                            @endforeach
+						</tbody>
+					  </table>
+					</div>
+				</div>
+				<!-- /.box-body -->
+			  </div>
+			  <!-- /.box -->      
+			</div>
+			<!-- /.col -->
+		  </div>
+		  <!-- /.row -->
+		</section>
+		<!-- /.content -->
+	  
+	  </div>
 
 @endsection
