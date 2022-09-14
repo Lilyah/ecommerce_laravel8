5c3ca9481e938cf2e9576c37ecf1dca2a524b416
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2022-09-14 12:43:09 +0300

    views/admin/admin_master.blade: Fixing typo in easypiechart script

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -54,7 +54,7 @@
 	<!-- Vendor JS -->
 	<script src="{{ asset('backend/js/vendors.min.js')}}"></script>
   <script src="{{ asset('assets/icons/feather-icons/feather.min.js')}}"></script>	
-	<script src="{{ asset('assets/vendor_components/easypiechart/dist/jquery.easypiechart.j')}}s"></script>
+	<script src="{{ asset('assets/vendor_components/easypiechart/dist/jquery.easypiechart.js')}}"></script>
 	<script src="{{ asset('assets/vendor_components/apexcharts-bundle/irregular-data-series.js')}}"></script>
 	<script src="{{ asset('assets/vendor_components/apexcharts-bundle/dist/apexcharts.js')}}"></script>
   <script src="{{ asset('../assets/vendor_components/datatable/datatables.min.js')}}"></script>
