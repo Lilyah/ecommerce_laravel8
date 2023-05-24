3daf7244e85372b576d17919d177ba2db05a2a7b
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2023-05-24 10:21:29 +0300

    /views/frontend/index.blade: Refactoring slider code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -215,44 +215,20 @@
         <div id="hero">
           <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
 
-          <!-- English Language -->
-          @if (session()->get('language') == 'english') 
-
-            @foreach ($sliders as $slider)
+              @foreach ($sliders as $slider)
             
-              <div class="item" style="background-image: url({{ asset($slider->slider_image) }});">
-                <div class="container-fluid">
-                  <div class="caption bg-color vertical-center text-left">
-                    <div class="slider-header fadeInDown-1"></div>
-                    <div class="big-text fadeInDown-1">{{ $slider->title_en }}</div>
-                    <div class="excerpt fadeInDown-2 hidden-xs"> <span>{{ $slider->description_en }}</span> </div>
-                    <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
-                  </div><!-- /.caption -->
-                </div><!-- /.container-fluid -->
-              </div><!-- /.item -->
+                <div class="item" style="background-image: url({{ asset($slider->slider_image) }});">
+                  <div class="container-fluid">
+                    <div class="caption bg-color vertical-center text-left">
+                      <div class="slider-header fadeInDown-1"></div>
+                      <div class="big-text fadeInDown-1">{{ (session()->get('language') == 'english') ? $slider->title_en : $slider->title_bg }}</div>
+                      <div class="excerpt fadeInDown-2 hidden-xs"> <span>{{ (session()->get('language') == 'english') ? $slider->description_en : $slider->description_bg }}</span> </div>
+                      <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">{{ (session()->get('language') == 'english') ? 'Shop Now' : 'ПАЗАРУВАЙ СЕГА' }}</a> </div>
+                    </div><!-- /.caption -->
+                  </div><!-- /.container-fluid -->
+                </div><!-- /.item -->
             
-            @endforeach
-          
-          @else  
-
-          <!-- Bulgarian Language -->
-
-          @foreach ($sliders as $slider)
-            
-            <div class="item" style="background-image: url({{ asset($slider->slider_image) }});">
-              <div class="container-fluid">
-                <div class="caption bg-color vertical-center text-left">
-                  <div class="slider-header fadeInDown-1"></div>
-                  <div class="big-text fadeInDown-1">{{ $slider->title_bg }}</div>
-                  <div class="excerpt fadeInDown-2 hidden-xs"> <span>{{ $slider->description_bg }}</span> </div>
-                  <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">ПАЗАРУВАЙ СЕГА</a> </div>
-                </div><!-- /.caption -->
-              </div><!-- /.container-fluid -->
-            </div><!-- /.item -->
-          
-          @endforeach
-
-        @endif
+              @endforeach
             
           </div>
           <!-- /.owl-carousel --> 
